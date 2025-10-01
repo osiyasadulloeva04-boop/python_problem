1e0a8504140fa544d3edf5723fef8c1e4fe6e8cf
osiyasadulloeva04-boop <osiyasadulloeva04@gmail.com>
2025-10-01 21:54:10 +0500

    Add files via upload

---
+++ python_task(1).py
@@ -0,0 +1,13 @@
+
+#Task 1
+names = ["Black", "Red", "Maroon", "Yellow"]
+codes = ["#000000", "#FF0000", "#800000", "#FFFF00"]
+result = []
+
+for i in range(4):
+    color = {"name": names[i], "code": codes[i]}
+    result.append(color)
+
+print(result)
+
+
